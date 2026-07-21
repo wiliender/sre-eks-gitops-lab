f1ed22e6e41df0bda524cff72c11f7ebd378fe32
Wiliender <wiliendersilva@gmail.com>
2026-07-21 02:01:13 -0300

    style(terraform): aplica terraform fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,9 +3,9 @@ terraform {
   # Define o S3 da AWS como o provedor de backend onde o arquivo .tfstate será armazenado
   backend "s3" {
     bucket         = "sre-eks-gitops-lab-wiliender-2026-tfstate" # Nome do bucket S3 único criado no bootstrap
-    key            = "dev/terraform.tfstate"                   # Caminho/nome do arquivo de estado dentro do bucket
-    region         = "us-east-1"                               # Região AWS onde o bucket S3 está localizado
-    encrypt        = true                                      # Garante a criptografia server-side (SSE-S3) do arquivo de estado
-    dynamodb_table = "terraform-lock"                          # Tabela DynamoDB para controle de trava (State Lock) contra concorrência
+    key            = "dev/terraform.tfstate"                     # Caminho/nome do arquivo de estado dentro do bucket
+    region         = "us-east-1"                                 # Região AWS onde o bucket S3 está localizado
+    encrypt        = true                                        # Garante a criptografia server-side (SSE-S3) do arquivo de estado
+    dynamodb_table = "terraform-lock"                            # Tabela DynamoDB para controle de trava (State Lock) contra concorrência
   }
 }
